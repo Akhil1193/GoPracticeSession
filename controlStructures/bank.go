ea10b1a497bbb5c0fbf065a15e22e33e58e64272
akhil <riyanreddy39@gmail.com>
2024-03-14 11:04:36 -0500

    added switch statments

--- controlStructures/bank.go
+++ controlStructures/bank.go
@@ -18,9 +18,10 @@ func main() {
 		fmt.Print("Enter your choice:")
 		fmt.Scan(&Choice)
 
-		if Choice == 1 {
+		switch Choice {
+		case 1:
 			fmt.Println("Your account balance is:", accoutBalance)
-		} else if Choice == 2 {
+		case 2:
 			fmt.Println("Your Deposit Amount:")
 			var addAmount float64
 			fmt.Scan(&addAmount)
@@ -30,7 +31,7 @@ func main() {
 			}
 			accoutBalance += addAmount
 			fmt.Println("Your total balance is:", accoutBalance)
-		} else if Choice == 3 {
+		case 3:
 			fmt.Println("Withdraw Amount:")
 			var WithdrawAmount float64
 			fmt.Scan(&WithdrawAmount)
@@ -45,11 +46,44 @@ func main() {
 			accoutBalance -= WithdrawAmount
 
 			fmt.Println("Your total balance is:", accoutBalance)
-		} else {
+		default:
 			fmt.Println("Good Bye!")
-			//return
-			break
+			fmt.Println("Thankyou for banking with us")
+			return
+			//break
 		}
+
+		// 	// if Choice == 1 {
+		// 	// 	fmt.Println("Your account balance is:", accoutBalance)
+		// 	// } else if Choice == 2 {
+		// 	// 	fmt.Println("Your Deposit Amount:")
+		// 	// 	var addAmount float64
+		// 	// 	fmt.Scan(&addAmount)
+		// 	// 	if addAmount <= 0 {
+		// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
+		// 	// 		continue
+		// 	// 	}
+		// 	// 	accoutBalance += addAmount
+		// 	// 	fmt.Println("Your total balance is:", accoutBalance)
+		// 	// } else if Choice == 3 {
+		// 	// 	fmt.Println("Withdraw Amount:")
+		// 	// 	var WithdrawAmount float64
+		// 	// 	fmt.Scan(&WithdrawAmount)
+		// 	// 	if WithdrawAmount <= 0 {
+		// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
+		// 	// 		continue
+		// 	// 	}
+		// 	// 	if WithdrawAmount > accoutBalance {
+		// 	// 		fmt.Println("Insufficient Amount in your Account!")
+		// 	// 		continue
+		// 	// 	}
+		// 	// 	accoutBalance -= WithdrawAmount
+
+		// 	// 	fmt.Println("Your total balance is:", accoutBalance)
+		// 	// } else {
+		// 	// 	fmt.Println("Good Bye!")
+		// 	// 	//return
+		// 	// 	break
+		// 	}
 	}
-	fmt.Println("Thankyou for banking with us")
 }
