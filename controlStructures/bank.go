edd239980b4aa12611e71368e98ccdbe4cb354ca
akhil <riyanreddy39@gmail.com>
2024-03-14 18:59:13 -0500

    resolved errors

--- controlStructures/bank.go
+++ controlStructures/bank.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
-func writeBalanceToFile(balance float64){
-	balanceText:=fmt.Sprint(balance)
-	os.WriteFile("balance.txt", []byte(balanceText), 0644 )
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
 }
 
 func main() {
@@ -26,81 +28,77 @@ func main() {
 
 		switch Choice {
 		case 1:
-		switch Choice {
-		case 1:
-			fmt.Println("Your account balance is:", accoutBalance)
-		case 2:
-		case 2:
-			fmt.Println("Your Deposit Amount:")
-			var addAmount float64
-			fmt.Scan(&addAmount)
-			if addAmount <= 0 {
-				fmt.Println("Invalid Amount! Amount should be greater than Zero")
-				continue
-			}
-			accoutBalance += addAmount
-			fmt.Println("Your total balance is:", accoutBalance)
-			writeBalanceToFile(accoutBalance)
-		case 3:
-			fmt.Println("Withdraw Amount:")
-			var WithdrawAmount float64
-			fmt.Scan(&WithdrawAmount)
-			if WithdrawAmount <= 0 {
-				fmt.Println("Invalid Amount! Amount should be greater than Zero")
-				continue
-			}
-			if WithdrawAmount > accoutBalance {
-				fmt.Println("Insufficient Amount in your Account!")
-				continue
-			}
-			accoutBalance -= WithdrawAmount
-			writeBalanceToFile(accoutBalance)
+			switch Choice {
+			case 1:
+				fmt.Println("Your account balance is:", accoutBalance)
+			case 2:
+				fmt.Println("Your Deposit Amount:")
+				var addAmount float64
+				fmt.Scan(&addAmount)
+				if addAmount <= 0 {
+					fmt.Println("Invalid Amount! Amount should be greater than Zero")
+					continue
+				}
+				accoutBalance += addAmount
+				fmt.Println("Your total balance is:", accoutBalance)
+				writeBalanceToFile(accoutBalance)
+			case 3:
+				fmt.Println("Withdraw Amount:")
+				var WithdrawAmount float64
+				fmt.Scan(&WithdrawAmount)
+				if WithdrawAmount <= 0 {
+					fmt.Println("Invalid Amount! Amount should be greater than Zero")
+					continue
+				}
+				if WithdrawAmount > accoutBalance {
+					fmt.Println("Insufficient Amount in your Account!")
+					continue
+				}
+				accoutBalance -= WithdrawAmount
+				writeBalanceToFile(accoutBalance)
 
-			fmt.Println("Your total balance is:", accoutBalance)
-		default:
-		default:
-			fmt.Println("Good Bye!")
-			fmt.Println("Thankyou for banking with us")
-			return
-			//break
-			fmt.Println("Thankyou for banking with us")
-			return
-			//break
+				fmt.Println("Your total balance is:", accoutBalance)
+			default:
+				fmt.Println("Good Bye!")
+				fmt.Println("Thankyou for banking with us")
+				return
+
+				//break
+			}
+
+			// 	// if Choice == 1 {
+			// 	// 	fmt.Println("Your account balance is:", accoutBalance)
+			// 	// } else if Choice == 2 {
+			// 	// 	fmt.Println("Your Deposit Amount:")
+			// 	// 	var addAmount float64
+			// 	// 	fmt.Scan(&addAmount)
+			// 	// 	if addAmount <= 0 {
+			// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
+			// 	// 		continue
+			// 	// 	}
+			// 	// 	accoutBalance += addAmount
+			// 	// 	fmt.Println("Your total balance is:", accoutBalance)
+			// 	// } else if Choice == 3 {
+			// 	// 	fmt.Println("Withdraw Amount:")
+			// 	// 	var WithdrawAmount float64
+			// 	// 	fmt.Scan(&WithdrawAmount)
+			// 	// 	if WithdrawAmount <= 0 {
+			// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
+			// 	// 		continue
+			// 	// 	}
+			// 	// 	if WithdrawAmount > accoutBalance {
+			// 	// 		fmt.Println("Insufficient Amount in your Account!")
+			// 	// 		continue
+			// 	// 	}
+			// 	// 	accoutBalance -= WithdrawAmount
+
+			// 	// 	fmt.Println("Your total balance is:", accoutBalance)
+			// 	// } else {
+			// 	// 	fmt.Println("Good Bye!")
+			// 	// 	//return
+			// 	// 	break
+			// 	}
 		}
-
-		// 	// if Choice == 1 {
-		// 	// 	fmt.Println("Your account balance is:", accoutBalance)
-		// 	// } else if Choice == 2 {
-		// 	// 	fmt.Println("Your Deposit Amount:")
-		// 	// 	var addAmount float64
-		// 	// 	fmt.Scan(&addAmount)
-		// 	// 	if addAmount <= 0 {
-		// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
-		// 	// 		continue
-		// 	// 	}
-		// 	// 	accoutBalance += addAmount
-		// 	// 	fmt.Println("Your total balance is:", accoutBalance)
-		// 	// } else if Choice == 3 {
-		// 	// 	fmt.Println("Withdraw Amount:")
-		// 	// 	var WithdrawAmount float64
-		// 	// 	fmt.Scan(&WithdrawAmount)
-		// 	// 	if WithdrawAmount <= 0 {
-		// 	// 		fmt.Println("Invalid Amount! Amount should be greater than Zero")
-		// 	// 		continue
-		// 	// 	}
-		// 	// 	if WithdrawAmount > accoutBalance {
-		// 	// 		fmt.Println("Insufficient Amount in your Account!")
-		// 	// 		continue
-		// 	// 	}
-		// 	// 	accoutBalance -= WithdrawAmount
-
-		// 	// 	fmt.Println("Your total balance is:", accoutBalance)
-		// 	// } else {
-		// 	// 	fmt.Println("Good Bye!")
-		// 	// 	//return
-		// 	// 	break
-		// 	}
-	}
 
 		// 	// if Choice == 1 {
 		// 	// 	fmt.Println("Your account balance is:", accoutBalance)
