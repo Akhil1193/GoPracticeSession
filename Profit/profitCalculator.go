ddb1f2a2ebe1cb6cd20564914073df910d05ace8
akhil <riyanreddy39@gmail.com>
2024-03-19 13:19:16 -0500

    worked on Go Essentials

--- Profit/profitCalculator.go
+++ Profit/profitCalculator.go
@@ -1,30 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+func writeDataToFile(ebt, profit, ratio float64) {
 
-	fmt.Print("revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("expense: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("taxRate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := (revenue - expenses) * (1 - taxRate)
-	incomeTaxExpense := ebt * taxRate
-	eat := ebt - incomeTaxExpense
-	ratio := ebt / eat
-
-	fmt.Println(ebt)
-	fmt.Println(incomeTaxExpense)
-	fmt.Println(eat)
-	fmt.Println(ratio)
+	fileName := "dataText.txt"
+	dataText := fmt.Sprintf("EBT:%.1f\nPROFIT:%.1f\nRATIO:%.3f\n",ebt, profit, ratio)
+	os.WriteFile(fileName, []byte(dataText), 0644)
 }
 
+func main() {
+	revenue, err1 := getUserInput("Revenue: ")
 
+	if err1 != nil{
+		return
+	}
+	expenses, err2 := getUserInput("Expenses: ")
+
+	if err2 !=nil{
+		panic(err2)
+	}
+
+	taxRate, err3 := getUserInput("Tax Rate: ")
+    if err3 !=nil{
+		panic(err3)
+	}
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	writeDataToFile(ebt, profit, ratio)
+
+	fmt.Printf("EBT is %.1f\n", ebt)
+	fmt.Printf("Profit is %.1f\n", profit)
+	fmt.Printf("Ratio is %.3f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) (float64, error) {
+	var userInput float64
+
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be positive number")
+	}
+
+	return userInput, nil
+}
